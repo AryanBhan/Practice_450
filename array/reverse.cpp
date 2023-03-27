dd1bdd98ccbb417ecb4a58ef55d08d80b89524cd
Aryan <theredsword70@gmail.com>
2023-03-28 00:12:44 +0530

    first commit

---
+++ array/reverse.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int low,high;
+    int arr[5]={1,2,3,4,5};
+    low =0; high=4;
+    while (low<high)
+    {
+        swap(arr[low],arr[high]);
+        low++;
+        high--;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        
+    }
+    
+    return 0;
+}
